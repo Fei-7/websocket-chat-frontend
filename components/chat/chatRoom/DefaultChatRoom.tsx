000ef220331cc07076d4acef23017ded83dfac96
Fei-7 <supachokang@gmail.com>
2024-04-23 11:47:14 +0700

    add: connect to websocket server even though the user not selected chatRoom

--- components/chat/chatRoom/DefaultChatRoom.tsx
+++ components/chat/chatRoom/DefaultChatRoom.tsx
@@ -1,7 +1,24 @@
+"use client"
 import Image from "next/image";
 import logoBlackBig from "@/public/logos/logo-black-big.svg";
+import { connect } from "@/websocket/clientSocket";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function DefaultChatRoom() {
+    useEffect(() => {
+        async function loadUserId() {
+            const me = await axios.get('http://localhost:3001/api/auth/me', {
+                withCredentials: true
+            })
+            console.log(me.data.data.id);
+            const userId = me.data.data.id;
+            connect("", userId);
+        }
+
+        loadUserId();
+    }, [])
+
     return (
         <div className="h-[80vh] w-full flex flex-col gap-6 justify-center items-center bg-neutral-100 border border-[#CBD5E1]">
             <Image
